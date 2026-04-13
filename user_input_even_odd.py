0a978a25ab8ac1c01ed8f3ff9b5aab79a0d60739
M. Sameer <mhatresameer@outlook.com>
2026-04-14 01:20:01 +0530

    Create user_input_even_odd.py

---
+++ user_input_even_odd.py
@@ -0,0 +1,11 @@
+# program 1
+# this is program to take user numeric input to check if the number is even or odd.
+
+# takes user input for any integer.
+user_input = int(input("Enter the number to check if it is EVEN or ODD: "))
+
+# loops for if-else
+if user_input % 2 = 0:
+  print("Entered number is EVEN")
+else:
+  print("Entered number is ODD")
